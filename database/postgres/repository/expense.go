5030e8cfac66fc8905909ee43a5094c6e46b2358
Victor <victorschumacher255@gmail.com>
2021-05-06 21:42:26 -0300

    saving data

---
+++ database/postgres/repository/expense.go
@@ -0,0 +1,48 @@
+package repository
+
+import (
+	"coletor-gastos-deputados/data"
+	"coletor-gastos-deputados/database"
+
+	"github.com/google/uuid"
+)
+
+type Expense interface {
+	Save(expense *data.Expense) error
+}
+
+type ExpenseRepo struct {
+	db database.DBConnection
+}
+
+func NewExpense(db database.DBConnection) ExpenseRepo {
+	return ExpenseRepo{db: db}
+}
+
+func (er ExpenseRepo) Save(expense *data.Expense) error {
+	db := er.db.ConnectHandle()
+	defer db.Close()
+	stmt := `INSERT INTO "coletor-gastos".deputados.gastos VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)`
+
+	id, err := uuid.NewRandom()
+	if err != nil {
+		return err
+	}
+
+	if _, err := db.Exec(
+		stmt,
+		id,
+		expense.Date,
+		expense.Legislatura,
+		expense.Partido,
+		expense.NomeParlamentar,
+		expense.CPFCNPJ,
+		expense.Description,
+		expense.Provider,
+		expense.Value,
+	); err != nil {
+		return err
+	}
+
+	return nil
+}
